1f61dfaf959e755542ab188c8122d6d262bd252e
Henry Doan <henrydoan96@gmail.com>
2016-05-12 20:41:27 -0600

    basic and bonus

--- magic_eight.rb
+++ magic_eight.rb
@@ -3,6 +3,7 @@
 # user can type quit to exit and says a message
 
 @answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may repy on it", "As I see it, yes", 'Most likely', 'Outlook good', 'Yes' 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+@easter_egg = ["JUST DO IT!", "Another one", "YOLO", "HASH => ROCKET!"]
 def menu
 	puts "Type a question to get an answer, or type q to exit."
 	choice = gets.strip.downcase
@@ -10,7 +11,21 @@ def menu
 		puts "Goodbye"
 		exit(0)
 	end
-	puts "#{@answers.sample}"
+	if choice == 'who do you love?'
+		puts "You found secret."
+		puts "#{@easter_egg.sample}"
+	elsif choice == 'Just do it'
+		puts "You found secret."
+		puts "#{@easter_egg.sample}"
+	elsif choice == 'talk dirty to me'
+		puts "You found secret."
+		puts "#{@easter_egg.sample}"
+	elsif choice == 'what time is it?'
+		puts "You found secret."
+		puts "#{@easter_egg.sample}"
+	else
+		puts "#{@answers.sample}"
+	end
 end
 
 while true
